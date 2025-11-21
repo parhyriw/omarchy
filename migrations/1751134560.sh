2bdf3fd450ec74a2259c729069d612c655398e36
David Heinemeier Hansson <david@hey.com>
2025-11-21 19:02:19 +0100

    Do all the upgrade work in that initial migration for old installations

--- migrations/1751134560.sh
+++ migrations/1751134560.sh
@@ -9,17 +9,18 @@ export OMARCHY_PATH=$HOME/.local/share/omarchy
 export PATH=$OMARCHY_PATH/bin/:$PATH
 EOF
 
-# Remove Zoom/qt5-remoteobjects to prevent a super lengthy build on old Omarchy installs
-sudo pacman -Rns --noconfirm zoom qt5-remoteobjects
-
-# Ensure we have the latest repos
-omarchy-refresh-pacman-mirrorlist
-if ! grep -q "omarchy" /etc/pacman.conf; then
-  sudo sed -i '/^\[core\]/i [omarchy]\nSigLevel = Optional TrustAll\nServer = https:\/\/pkgs.omarchy.org\/stable\/$arch\n' /etc/pacman.conf
-fi
+# Ensure we have the latest repos and are ready to pull
+omarchy-refresh-pacman
+sudo systemctl restart systemd-timesyncd
 
 mkdir -p ~/.local/state/omarchy/migrations
 touch ~/.local/state/omarchy/migrations/1751134560.sh
 
-sudo systemctl restart systemd-timesyncd
+# Remove Zoom/qt5-remoteobjects to prevent a super lengthy build on old Omarchy installs
+sudo pacman -Rns --noconfirm zoom qt5-remoteobjects
+
+# Get rid of old AUR packages
+bash $OMARCHY_PATH/migrations/1756060611.sh
+touch ~/.local/state/omarchy/migrations/1756060611.sh
+
 bash omarchy-update-perform
--- migrations/1751134562.sh
+++
@@ -1,4 +0,0 @@
-echo "Ensure all indexes and packages are up to date"
-
-omarchy-refresh-pacman-mirrorlist
-sudo pacman -Syu --noconfirm
