d87f0aaa193b5b01bda26b93c938c8e85b4c6026
David Heinemeier Hansson <david@hey.com>
2025-11-21 18:49:34 +0100

    Improve the update for old installs

--- migrations/1751134560.sh
+++ migrations/1751134560.sh
@@ -9,6 +9,15 @@ export OMARCHY_PATH=$HOME/.local/share/omarchy
 export PATH=$OMARCHY_PATH/bin/:$PATH
 EOF
 
+# Remove Zoom/qt5-remoteobjects to prevent a super lengthy build on old Omarchy installs
+sudo pacman -Rns --noconfirm zoom qt5-remoteobjects
+
+# Ensure we have the latest repos
+omarchy-refresh-pacman-mirrorlist
+if ! grep -q "omarchy" /etc/pacman.conf; then
+  sudo sed -i '/^\[core\]/i [omarchy]\nSigLevel = Optional TrustAll\nServer = https:\/\/pkgs.omarchy.org\/stable\/$arch\n' /etc/pacman.conf
+fi
+
 mkdir -p ~/.local/state/omarchy/migrations
 touch ~/.local/state/omarchy/migrations/1751134560.sh
 
