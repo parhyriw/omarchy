abffb0abfe39cb99ab62235894ad5c15e88e2842
Ryan Hughes <ryan@heyoodle.com>
2025-12-13 00:42:13 -0500

    Add hyprland-preview-share-picker

---
+++ migrations/1765587218.sh
@@ -0,0 +1,6 @@
+echo "Add custom share portal picker"
+omarchy-pkg-add hyprland-preview-share-picker-git
+
+if ! grep -q "custom_picker_binary" ~/.config/hypr/xdph.conf; then
+    sed -i '/screencopy {/a\    custom_picker_binary = hyprland-preview-share-picker' ~/.config/hypr/xdph.conf
+fi
