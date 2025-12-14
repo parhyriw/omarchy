457b954db58490109bc7f69f8b06613becd72f84
Ryan Hughes <ryan@heyoodle.com>
2025-12-14 15:19:25 -0500

    Move module to a separate file

--- install/login/limine-snapper.sh
+++ install/login/limine-snapper.sh
@@ -2,8 +2,10 @@ if command -v limine &>/dev/null; then
   sudo pacman -S --noconfirm --needed limine-snapper-sync limine-mkinitcpio-hook
 
   sudo tee /etc/mkinitcpio.conf.d/omarchy_hooks.conf <<EOF >/dev/null
-MODULES=(thunderbolt)
 HOOKS=(base udev plymouth keyboard autodetect microcode modconf kms keymap consolefont block encrypt filesystems fsck btrfs-overlayfs)
+EOF
+  sudo tee /etc/mkinitcpio.conf.d/thunderbolt_module.conf <<EOF >/dev/null
+MODULES+=(thunderbolt)
 EOF
 
   [[ -f /boot/EFI/limine/limine.conf ]] || [[ -f /boot/EFI/BOOT/limine.conf ]] && EFI=true
