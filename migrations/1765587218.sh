33eabb0c888e68e7b880a59450c4198e17e5d3fb
Ryan Hughes <ryan@heyoodle.com>
2025-12-13 20:29:31 -0500

    Change to non-git version

--- migrations/1765587218.sh
+++ migrations/1765587218.sh
@@ -1,5 +1,5 @@
 echo "Add custom share portal picker"
-omarchy-pkg-add hyprland-preview-share-picker-git
+omarchy-pkg-add hyprland-preview-share-picker
 
 if ! grep -q "custom_picker_binary" ~/.config/hypr/xdph.conf; then
     sed -i '/screencopy {/a\    custom_picker_binary = hyprland-preview-share-picker' ~/.config/hypr/xdph.conf
