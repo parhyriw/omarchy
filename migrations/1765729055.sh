88e9c9db49e993dd7e4a9a82145a9dff07d18621
Ryan Hughes <ryan@heyoodle.com>
2025-12-14 14:19:37 -0500

    Tell the user what we're doing

--- migrations/1765729055.sh
+++ migrations/1765729055.sh
@@ -1,3 +1,4 @@
+echo "Add emergency entry for Walker"
 CONFIG_FILE="$HOME/.config/walker/config.toml"
 
 if [[ -f "$CONFIG_FILE" ]] && ! grep -q 'command = "omarchy-restart-walker"' "$CONFIG_FILE"; then
