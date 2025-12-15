92af9f41f2fbe304ca774fd577186a31fbe30087
Ryan Hughes <ryan@heyoodle.com>
2025-12-15 12:38:28 -0500

    Reorder

--- migrations/1765587218.sh
+++ migrations/1765587218.sh
@@ -1,17 +1,17 @@
 echo "Add custom share portal picker"
 omarchy-pkg-add hyprland-preview-share-picker
 
-if ! grep -q "custom_picker_binary" ~/.config/hypr/xdph.conf; then
-  sed -i '/screencopy {/a\    custom_picker_binary = hyprland-preview-share-picker' ~/.config/hypr/xdph.conf
-
-  sleep 1
-  killall -e xdg-desktop-portal-hyprland
-  killall -e xdg-desktop-portal-wlr
-  killall xdg-desktop-portal
-  /usr/lib/xdg-desktop-portal-hyprland &
-  sleep 2
-  /usr/lib/xdg-desktop-portal &
-fi
-
 mkdir -p ~/.config/hyprland-preview-share-picker
 omarchy-refresh-config hyprland-preview-share-picker/config.yaml
+
+if ! grep -q "custom_picker_binary" ~/.config/hypr/xdph.conf; then
+  sed -i '/screencopy {/a\    custom_picker_binary = hyprland-preview-share-picker' ~/.config/hypr/xdph.conf
+fi
+
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
