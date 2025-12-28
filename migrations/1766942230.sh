2c7480135a1020642074fac7db4d14a99f59eb1e
Ryan Hughes <ryan@heyoodle.com>
2025-12-28 13:27:33 -0500

    Add migration

--- migrations/1766942230.sh
+++ migrations/1766942230.sh
@@ -3,6 +3,6 @@ echo "Migrate legacy NVIDIA GPUs to nvidia-580xx driver (if needed)"
 # Only migrate GTX 9xx or 10xx (Pascal/Maxwell)
 NVIDIA="$(lspci | grep -i 'nvidia')"
 if echo "$NVIDIA" | grep -qE "GTX 9|GTX 10"; then
-  omarchy-pkg-drop nvidia nvidia-dkms nvidia-open-dkms nvidia-utils lib32-nvidia-utils
-  omarchy-pkg-add nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils
+  # Piping yes to override existing packages
+  yes | sudo pacman -S nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils
 fi
