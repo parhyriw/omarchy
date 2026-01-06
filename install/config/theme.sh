3a4536059ada92f66d274c44b400e9fbfeb80ec9
Reno <gillet.ren@gmail.com>
2026-01-06 16:50:16 +0100

    fix : typo (Nautilius -> Nautilus) (#4098)

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -1,4 +1,4 @@
-# Set links for Nautilius action icons
+# Set links for Nautilus action icons
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
 
