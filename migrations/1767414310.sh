df3d07e41f89e89fe177931666d9722652afe0a0
David Heinemeier Hansson <david@hey.com>
2026-01-06 13:55:52 +0100

    Fix timeout correction

--- migrations/1767414310.sh
+++ migrations/1767414310.sh
@@ -1,4 +1,3 @@
-echo "Use correct idle-timer sensitive timeouts for screensaver + screen off"
+echo "Use correct idle-timer sensitive timeouts for lock screen"
 
-sed -i 's/timeout = 300/timeout = 150/' ~/.config/hypr/hypridle.conf
-sed -i 's/timeout = 330/timeout = 30/' ~/.config/hypr/hypridle.conf
+sed -i 's/timeout = 300/timeout = 151/' ~/.config/hypr/hypridle.conf
