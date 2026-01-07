d207d505f5eecdf86b5ef4f37debfc280d1af197
Ryan Hughes <ryan@heyoodle.com>
2026-01-07 11:19:53 -0500

    Add initial omarchy skill for OpenCode (and friends) (#4090)

--- migrations/1767478687.sh
+++ migrations/1767478687.sh
@@ -7,11 +7,3 @@ if [[ ! -f ~/.config/opencode/opencode.json ]]; then
   mkdir -p ~/.config/opencode
   cp $OMARCHY_PATH/config/opencode/opencode.json ~/.config/opencode/opencode.json
 fi
-
-# Add binding if the key is available
-BINDINGS_PATH="$HOME/.config/hypr/bindings.conf"
-if ! grep -q "SUPER SHIFT CTRL, A" $BINDINGS_PATH; then
-  sed -i '/SUPER SHIFT ALT, A/a bindd = SUPER SHIFT CTRL, A, opencode, exec, omarchy-launch-opencode' "$BINDINGS_PATH"
-else
-  echo "Add your own binding for opencode using omarchy-launch-opencode in $BINDINGS_PATH"
-fi
