cd8a6430e2e6c059251fcdeac69bb4a395905940
David Heinemeier Hansson <david@hey.com>
2026-01-07 19:14:32 +0100

    Use edge mirror for dev online installs

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -4,7 +4,12 @@ if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
 
   # Configure pacman
   sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
-  sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist-stable /etc/pacman.d/mirrorlist
+
+  if [[ ${OMARCHY_REF:-} == "dev" ]]; then
+    sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist-edge /etc/pacman.d/mirrorlist
+  else
+    sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist-stable /etc/pacman.d/mirrorlist
+  fi
 
   sudo pacman-key --recv-keys 40DFB630FF42BCFFB047046CF0134EE680CAC571 --keyserver keys.openpgp.org
   sudo pacman-key --lsign-key 40DFB630FF42BCFFB047046CF0134EE680CAC571
