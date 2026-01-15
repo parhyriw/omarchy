5ff76df5e4b86da2923a7dd38cc565637ff9170a
David Heinemeier Hansson <david@hey.com>
2026-01-14 19:53:28 -0500

    Ensure default routes don't overwrite the ffix

--- migrations/1768429270.sh
+++ migrations/1768429270.sh
@@ -2,4 +2,5 @@ echo "Fix volume controls for laptops with problematic Realtek codecs (like Asus
 
 mkdir -p ~/.config/wireplumber/wireplumber.conf.d/
 cp $OMARCHY_PATH/config/wireplumber/wireplumber.conf.d/alsa-soft-mixer.conf ~/.config/wireplumber/wireplumber.conf.d/
+rm ~/.local/state/wireplumber/default-routes
 systemctl --user restart wireplumber
