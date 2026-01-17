21514dc577cb786c357eeb779eb708db71762a88
David Heinemeier Hansson <david@hey.com>
2026-01-17 14:43:59 -0500

    Turn audio soft mixing into an opt-in toggle
    It causes issues on some machines, but is required on the Asus G14

--- migrations/1768429270.sh
+++
@@ -1,6 +0,0 @@
-echo "Fix volume controls for laptops with problematic Realtek codecs (like Asus G14)"
-
-mkdir -p ~/.config/wireplumber/wireplumber.conf.d/
-cp $OMARCHY_PATH/config/wireplumber/wireplumber.conf.d/alsa-soft-mixer.conf ~/.config/wireplumber/wireplumber.conf.d/
-rm ~/.local/state/wireplumber/default-routes
-systemctl --user restart wireplumber
