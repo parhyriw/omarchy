0930583526b2241ab98d86149e38761a8c659db2
David Heinemeier Hansson <david@hey.com>
2026-01-20 16:12:51 -0400

    Fix sourcing

--- migrations/1768916735.sh
+++ migrations/1768916735.sh
@@ -1,3 +1,3 @@
 echo "Fix microphone gain on Asus ROG laptops with ALC285 Realtek"
 
-$OMARCHY_PATH/install/config/hardware/fix-asus-rog-mic.sh
+source "$OMARCHY_PATH/install/config/hardware/fix-asus-rog-mic.sh"
