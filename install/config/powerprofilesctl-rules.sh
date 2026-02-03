4b3e21445ba7c539050f34d04c81a119b139f8b6
Pierre Olivier Martel <pomartel@coderubik.com>
2026-02-03 10:18:24 -0500

    Don't apply power profile source switching if only one profile is available (#4485)

--- install/config/powerprofilesctl-rules.sh
+++ install/config/powerprofilesctl-rules.sh
@@ -1,19 +1,15 @@
 if omarchy-battery-present; then
   mapfile -t profiles < <(omarchy-powerprofiles-list)
 
-  if [[ ${#profiles[@]} -gt 0 ]]; then
+  if [[ ${#profiles[@]} -gt 1 ]]; then
 
     # Default AC profile:
     # 3 profiles → performance
     # 2 profiles → balanced
-    # 1 profile  → profiles[0]
-    ac_profile="${profiles[2]:-${profiles[1]:-${profiles[0]}}}"
+    ac_profile="${profiles[2]:-${profiles[1]}}"
 
-    # Default Battery profile:
-    # 3 profiles → balanced
-    # 2 profiles → balanced
-    # 1 profile  → profiles[0]
-    battery_profile="${profiles[1]:-${profiles[0]}}"
+    # Default Battery profile (balanced)
+    battery_profile="${profiles[1]}"
 
     cat <<EOF | sudo tee "/etc/udev/rules.d/99-power-profile.rules"
 SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/powerprofilesctl set $battery_profile"
