7bd5bf25cd8679e546c7b8217ba9ca0c02767fbb
bjarneo <bjarneo@users.noreply.github.com>
2026-02-07 22:16:21 +0100

    Remove the aether.nvim dependency (#4543)
    * Remove the aether.nvim dependency
    
    * Fix a blooper

--- themes/vantablack/neovim.lua
+++ themes/vantablack/neovim.lua
@@ -2,10 +2,6 @@ return {
   {
     "bjarneo/vantablack.nvim",
     priority = 1000,
-    dependencies = {
-      "bjarneo/aether.nvim",
-      branch = "v2"
-    },
   },
   {
     "LazyVim/LazyVim",
