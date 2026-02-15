9aff0d4b3b5451bd1637c5b2176645a07a4aadf8
[1;13D[1;11D[1;7DDavid Heinemeier Hansson <david@hey.com>
2026-02-15 23:06:07 +0100

    Note about consequences

--- migrations/1769510847.sh
+++ migrations/1769510847.sh
@@ -1,4 +1,5 @@
 echo "Switch back to mainline chromium now that it supports full live themeing"
+echo "Note: This required resetting cookies and settings!"
 
 omarchy-pkg-drop omarchy-chromium
 omarchy-pkg-add chromium
