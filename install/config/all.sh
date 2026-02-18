43ae186a80683dc3091773e0720936ebcb5af399
Steve Godlewski <steve.w.godlewski@gmail.com>
2026-02-18 13:19:30 -0700

    Add Framework 16 RGB keyboard theme syncing (#4524)
    * Add framwork 16 RGB syncing
    
    * fixed install script, fixed color changing when style changes
    
    * added framework scripts to the main install scripts
    
    * renamed theme-set scripts, added migration
    
    * use omarchy-pkg-add, shared keyboard template, tracked udev rule
    
    * call the install file from the migration file

--- install/config/all.sh
+++ install/config/all.sh
@@ -41,3 +41,4 @@ run_logged $OMARCHY_INSTALL/config/hardware/fix-asus-rog-audio-mixer.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-asus-rog-mic.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-yt6801-ethernet-adapter.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-synaptic-touchpad.sh
+run_logged $OMARCHY_INSTALL/config/hardware/framework16-qmk-hid.sh
