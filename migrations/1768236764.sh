864df215bba877ff80d159cc6fe5f2929cea93bc
David Heinemeier Hansson <david@hey.com>
2026-02-20 21:58:00 +0100

    Improve description

--- migrations/1768236764.sh
+++ migrations/1768236764.sh
@@ -1,4 +1,4 @@
-echo "Install kernel-modules-hook"
+echo "Prevent kernel upgrades from making current modules unavailable"
 
 omarchy-pkg-add kernel-modules-hook
 sudo systemctl enable --now linux-modules-cleanup.service
