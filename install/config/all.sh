694eb8c6982cd781e091fbf4aaea0e31bd5d486e
David Heinemeier Hansson <david@hey.com>
2026-02-20 18:12:54 +0100

    Install vulkan drivers by default
    Closes #2926
    Co-authored-by: @ElBrodino

--- install/config/all.sh
+++ install/config/all.sh
@@ -32,6 +32,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 run_logged $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 run_logged $OMARCHY_INSTALL/config/hardware/legacy-gpu-terminal.sh
 run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
+run_logged $OMARCHY_INSTALL/config/hardware/vulkan.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-bcm43xx.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-spi-keyboard.sh
