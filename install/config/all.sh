e6f7c94fcf3981682061debb8428a1e09c0a5e25
Stefan Gründel <stefan.gruendel@gmail.com>
2026-02-21 16:51:25 +0100

    Remove legacy GPU terminal script and its invocation

--- install/config/all.sh
+++ install/config/all.sh
@@ -31,7 +31,6 @@ run_logged $OMARCHY_INSTALL/config/hardware/bluetooth.sh
 run_logged $OMARCHY_INSTALL/config/hardware/printer.sh
 run_logged $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 run_logged $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
-run_logged $OMARCHY_INSTALL/config/hardware/legacy-gpu-terminal.sh
 run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
 run_logged $OMARCHY_INSTALL/config/hardware/vulkan.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
