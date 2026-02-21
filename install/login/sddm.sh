1a743d34673ce61966e8a8c876ceb1a8dc6533da
David Heinemeier Hansson <david@hey.com>
2026-02-21 15:03:16 +0100

    Prevent password-based SDDM logins from creating an encrypted login keyring

--- install/login/sddm.sh
+++ install/login/sddm.sh
@@ -14,5 +14,10 @@ Current=omarchy
 EOF
 fi
 
+# Prevent password-based SDDM logins from creating an encrypted login keyring
+# (which conflicts with the passwordless Default_keyring used for auto-unlock)
+sudo sed -i '/-auth.*pam_gnome_keyring\.so/d' /etc/pam.d/sddm
+sudo sed -i '/-password.*pam_gnome_keyring\.so/d' /etc/pam.d/sddm
+
 # Don't use chrootable here as --now will cause issues for manual installs
 sudo systemctl enable sddm.service
