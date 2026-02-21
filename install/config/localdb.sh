262fadd5314c1394abb936abf2ce5c77a51dbc2a
David Heinemeier Hansson <david@hey.com>
2026-02-21 15:32:15 +0100

    Revert "Ensure /home is indexed on Btrfs"
    This reverts commit 9f561e0123b0a2266ca35442fefa5a9290d1e898.

--- install/config/localdb.sh
+++ install/config/localdb.sh
@@ -1,3 +1,2 @@
-# Ensure /home is indexed on Btrfs (subvolumes look like bind mounts)
-sudo sed -i 's/PRUNE_BIND_MOUNTS.*=.*/PRUNE_BIND_MOUNTS = "no"/' /etc/updatedb.conf
+# Update localdb so that locate will find everything installed
 sudo updatedb
--- migrations/1771622309.sh
+++
@@ -1,3 +0,0 @@
-echo "Fix locate not indexing home on Btrfs"
-sudo sed -i 's/PRUNE_BIND_MOUNTS.*=.*/PRUNE_BIND_MOUNTS = "no"/' /etc/updatedb.conf
-sudo updatedb
