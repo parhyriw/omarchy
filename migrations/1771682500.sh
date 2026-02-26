24e7afefc5856685797f02e63602ce6872f9481a
David Heinemeier Hansson <david@hey.com>
2026-02-26 10:20:32 +0100

    Backup login.keyring instead of just deleting it

--- migrations/1771682500.sh
+++ migrations/1771682500.sh
@@ -1,7 +1,9 @@
 echo "Prevent SDDM password login from creating encrypted login keyring"
 
-# Remove the encrypted login keyring if it exists (it conflicts with the passwordless Default_keyring)
-rm -f "$HOME/.local/share/keyrings/login.keyring"
+# Rename the encrypted login keyring if it exists (it conflicts with the passwordless Default_keyring)
+if [[ -f $HOME/.local/share/keyrings/login.keyring ]]; then
+  mv "$HOME/.local/share/keyrings/login.keyring" "$HOME/.local/share/keyrings/login.keyring.bak"
+fi
 
 # Remove gnome-keyring auth/password lines from sddm PAM so password-based logins
 # don't create an encrypted login keyring. Keep the session line to start the daemon,
