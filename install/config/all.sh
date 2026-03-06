2a2e9086e1c87cb2673c7196df35d841ae7229e4
David Heinemeier Hansson <david@hey.com>
2026-03-06 21:58:04 +0100

    We weren't running this for some reason!

--- install/config/all.sh
+++ install/config/all.sh
@@ -33,6 +33,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/printer.sh
 run_logged $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 run_logged $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
+run_logged $OMARCHY_INSTALL/config/hardware/intel.sh
 run_logged $OMARCHY_INSTALL/config/hardware/vulkan.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-intel-panther-lake-display.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
