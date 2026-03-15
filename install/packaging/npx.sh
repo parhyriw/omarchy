948bac9f6cd1baba9853cf929e3961cfe3019ccc
David Heinemeier Hansson <david@hey.com>
2026-03-15 19:19:03 +0100

    Fix install of playwright

--- install/packaging/npx.sh
+++ install/packaging/npx.sh
@@ -3,4 +3,4 @@ omarchy-npx-install @openai/codex codex
 omarchy-npx-install @google/gemini-cli gemini
 omarchy-npx-install @github/copilot copilot
 omarchy-npx-install opencode-ai opencode
-omarchy-npx-install playwright-cli
+omarchy-npx-install playwright playwright-cli
