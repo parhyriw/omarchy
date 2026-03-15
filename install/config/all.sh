2d35b98bbb20cd557f2c48062dea3d5c77958505
David Heinemeier Hansson <david@hey.com>
2026-03-15 12:13:26 +0100

    Thermald helps manage thermal load on Intel laptops with Sandy Bridge or newer (#5022)
    Thanks to the team at Intel for pointing this out 👌

--- install/config/all.sh
+++ install/config/all.sh
@@ -35,6 +35,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 run_logged $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
 run_logged $OMARCHY_INSTALL/config/hardware/intel.sh
+run_logged $OMARCHY_INSTALL/config/hardware/intel-thermald.sh
 run_logged $OMARCHY_INSTALL/config/hardware/vulkan.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-intel-panther-lake-display.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
