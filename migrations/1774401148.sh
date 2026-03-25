f26df5adf5c7f9fd43ca1f58415c529e4561a2c4
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-03-25 22:41:19 +0900

    Fix: Limine drop-in config are overridden by default limine conf (#5114)
    Limine drop-in config are overridden by `/etc/default/limine`

---
+++ migrations/1774401148.sh
@@ -0,0 +1,5 @@
+echo "Fix KERNEL_CMDLINE merge behavior in /etc/default/limine"
+
+if [[ -f /etc/default/limine ]]; then
+  sudo sed -i 's/^KERNEL_CMDLINE\[default\]="/KERNEL_CMDLINE[default]+="/' /etc/default/limine
+fi
