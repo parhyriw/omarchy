22e84776a8c3d2c2bd21de794cf4e58c6f644818
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-03-26 15:47:15 +0900

    Fix missing limine-mkinitcpio in migration script (#5117)
    Follow up to #5114, I forgot to regenerate the `/boot/limine.conf` file during the migration.

--- migrations/1774401148.sh
+++ migrations/1774401148.sh
@@ -2,4 +2,5 @@ echo "Fix KERNEL_CMDLINE merge behavior in /etc/default/limine"
 
 if [[ -f /etc/default/limine ]]; then
   sudo sed -i 's/^KERNEL_CMDLINE\[default\]="/KERNEL_CMDLINE[default]+="/' /etc/default/limine
+  sudo limine-mkinitcpio
 fi
