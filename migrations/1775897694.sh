9389c1f57ad49de12abeb9c1e8ef7048f08725cf
David Heinemeier Hansson <david@hey.com>
2026-04-15 10:39:21 +0200

    Fix path

--- migrations/1775897694.sh
+++ migrations/1775897694.sh
@@ -1,4 +1,4 @@
 echo "Fix AC power udev rules to use systemd-run"
 
-source $OMARCHY_PATH/install/first-run/powerprofilesctl-rules.sh
+source $OMARCHY_PATH/install/config/powerprofilesctl-rules.sh
 source $OMARCHY_PATH/install/config/wifi-powersave-rules.sh
