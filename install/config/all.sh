f7dc63ca72ebb361b78ae284ff47bed6b42bb7e5
David Heinemeier Hansson <david@hey.com>
2026-04-15 02:26:37 -0700

    Work around the return-from-sleep sluggishness on panther lake
    This is due to slow ramp after s2idle

--- install/config/all.sh
+++ install/config/all.sh
@@ -43,6 +43,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/intel/thermald.sh
 run_logged $OMARCHY_INSTALL/config/hardware/intel/ipu7-camera.sh
 run_logged $OMARCHY_INSTALL/config/hardware/intel/ptl-kernel.sh
 run_logged $OMARCHY_INSTALL/config/hardware/intel/fix-wifi7-eht.sh
+run_logged $OMARCHY_INSTALL/config/hardware/intel/resume-boost.sh
 
 run_logged $OMARCHY_INSTALL/config/hardware/dell/fix-xps-haptic-touchpad.sh
 run_logged $OMARCHY_INSTALL/config/hardware/dell/fix-xps-ptl-display.sh
